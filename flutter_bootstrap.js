d4ee3ac4c224cc74b10f533d7054d6e4c4457a83
udaykhalsa <udaykhalsa@gmail.com>
2024-12-26 11:55:29 +0530

    New Changes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"36335019a8eab588c3c2ea783c618d90505be2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "419337938"
+    serviceWorkerVersion: "4284206875"
   }
 });
